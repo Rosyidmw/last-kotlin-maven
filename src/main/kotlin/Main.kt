478ace65ecaec397b8af64e6c8d0bcbdcf4f05ad
Rosyid Mukti Wibowo <115602369+Rosyidmw@users.noreply.github.com>
2025-06-09 09:04:28 +0700

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 // Main.kt
-fun add(a: Int, b: Int): Int {
+fun add(a: Int, b: Int): Int? {
+    if (a < 0 || b < 0) {
+        return null // berpotensi NullPointerException jika tidak di-handle
+    }
     return a + b
 }
 
 fun main() {
     println("Program Penjumlahan Sederhana")
-    val a = 15
+    val a = -1
     val b = 10
     val c = add(a, b)
     println(c)
